cad4c5935172621138b3473d1484c9d26af7f21e
nabihah <nabihah.irfan@outlook.com>
2025-06-02 01:00:47 +0300

    SS-43: Created Forge webtrigger to store metrics

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -14,37 +14,22 @@ app.get('/test', async(req, res) => {
 })
 
 app.post('/metrics', async (req, res) => {
-  const { measurements } = req.body;
-  console.log('Metrics Received: ', measurements);
+  const metrics = req.body;
+  const data = JSON.stringify(metrics);
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: process.env.FROGE_METRICS_WEB_TRIGGER_URL,
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    data
+  };
 
   try {
-    const forgeRequest = {
-      method: 'POST',
-      path: '/x1/BwRYiiZYkCKCWxX1Qoy6Z9uQmZ8',
-      headers: {
-        "Content-Type": ["application/json"]
-      },
-      body: JSON.stringify({
-        key: 'submitMetrics',
-        payload: measurements
-      }),
-      // Optional: add query parameters if needed
-      queryParameters: {} 
-    };
-
-    console.log(forgeRequest);
-    const response = await axios.post(
-      `https://a5d4b6d8-65a7-47db-b69a-5f91ff6999d9.hello.atlassian-dev.net/x1/BwRYiiZYkCKCWxX1Qoy6Z9uQmZ8`,
-      forgeRequest,
-      {
-        headers: {
-          // Authorization: `Bearer ${process.env.FORGE_API_TOKEN}`,
-          'Content-Type': 'application/json'
-        }
-      }
-    );
-
-    res.status(200).json({ status: 'Forwarded', forgeResponse: response.statusCode });
+    const response = await axios.request(config);
+    console.log(JSON.stringify(response.data?.message));
+    res.status(200).json({ status: response.status, message: 'Forwarded Metrics to Forge Web Trigger' });
   } catch (error) {
     console.error('❗ Error forwarding to Forge:', error?.response?.data || error.message);
     res.status(500).json({ error: 'Failed to forward to Forge', details: error?.response?.data || error.message });
